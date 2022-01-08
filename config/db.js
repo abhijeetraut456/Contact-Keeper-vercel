15b6d9d09563b4c363044175e5fc223bf4aa775e
Abhijeet raut <Abhijeetraut456@gmail.com>
2022-01-08 19:13:46 +0530

    deploy

--- config/db.js
+++ config/db.js
@@ -4,7 +4,11 @@ const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db), console.log('MongoDb connected');
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
+      console.log('MongoDb connected');
   } catch (err) {
     console.error(err.message);
     process.exit(1);
