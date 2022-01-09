5cfe48d0069ba5a6f575978578b71239b3d2ecc1
Abhijeet raut <Abhijeetraut456@gmail.com>
2022-01-09 11:57:23 +0530

    deploy

--- config/db.js
+++ config/db.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const db = config.get('mongoURI');
+// const db = config.get('mongoURI');
+const { mongoURI } = require('./key');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, {
+    await mongoose.connect(mongoURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     }),
---
+++ config/development.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    'mongodb+srv://abhijeet:abhijeet@contactkepper.n154m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  jwtSecret: 'secret',
+};
